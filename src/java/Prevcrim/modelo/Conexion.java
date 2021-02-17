bc99e38376ef8be9fea82dced568b6c867e4cffe
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-02-17 01:38:16 -0300

    clase de conexion a la base de datos.

---
+++ src/java/Prevcrim/modelo/Conexion.java
@@ -0,0 +1,26 @@
+package Prevcrim.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ites_
+ * @version 1.0
+ */
+public class Conexion extends BBDD {
+     public Connection conn;
+    
+//    Constructor
+    public Conexion() {
+        try {
+            Class.forName(BBDD.CLASSNAME);
+            conn = DriverManager.getConnection(BBDD.URL, BBDD.USERNAME, BBDD.PASSWORD);
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println("Error, "+ex);
+        }
+    }
+   
+}
