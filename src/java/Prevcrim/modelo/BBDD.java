4e12966ba243cfd83613d018d347d44a78af9d26
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-02-17 01:36:51 -0300

    settings of configuration for conecction Base of Data

---
+++ src/java/Prevcrim/modelo/BBDD.java
@@ -0,0 +1,29 @@
+package Prevcrim.modelo;
+
+/**
+ *
+ * @author ites_
+ * @verson 1.0
+ */
+public class BBDD {
+    
+    /** Conexion a PostgreSQL */
+    public static final String HOST = "localhost";
+    public static final String PORT = "5432";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String DATABASE = "";
+    public static final String CLASSNAME= ""; // Driver del DBMS
+    public static final String URL = "";
+    
+    
+    /** Conexion a MySQL */
+//    public static final String HOST = "localhost";
+//    public static final String PORT = "3306";
+//    public static final String USERNAME = "";
+//    public static final String PASSWORD = "";
+//    public static final String DATABASE = "";
+//    public static final String CLASSNAME= ""; // Driver del DBMS
+//    public static final String URL = "";
+    
+}
