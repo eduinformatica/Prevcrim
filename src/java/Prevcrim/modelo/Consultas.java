73d83c081bfc0c1e4e3d16151292e56565917008
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-02-17 01:39:04 -0300

    clase consultas, donde se implementara scripts sql

---
+++ src/java/Prevcrim/modelo/Consultas.java
@@ -0,0 +1,17 @@
+package Prevcrim.modelo;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ites_
+ * @version 1.0
+ */
+public class Consultas {
+    
+    public boolean Authentication(String usuario, String clave) throws SQLException 
+    {
+        
+        return false;
+    }
+}
